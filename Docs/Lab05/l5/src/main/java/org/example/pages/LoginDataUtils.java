3c3d2a97b9b4040c92451efb8c300c80d28feeda
ursul113 <optrico@gmail.com>
2025-05-13 11:47:14 +0300

    last lab

---
+++ Docs/Lab05/l5/src/main/java/org/example/pages/LoginDataUtils.java
@@ -0,0 +1,39 @@
+package org.example.pages;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.DataProvider;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LoginDataUtils {
+    @DataProvider(name="loginData1")
+    public static Object[][] getLoginData1() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginData[] loginData = objectMapper.readValue(new File("resources/data.json"), LoginData[].class);
+        String username = loginData[0].getUsername();
+        String password = loginData[0].getPassword();
+        return new Object[][]{{username, password}};
+    }
+
+    @DataProvider(name="loginData2")
+    public static Object[][] getLoginData2() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginData[] loginData = objectMapper.readValue(new File("resources/data.json"), LoginData[].class);
+        String username = loginData[1].getUsername();
+        String password = loginData[1].getPassword();
+        return new Object[][]{{username, password}};
+    }
+
+    @DataProvider(name="loginData3")
+    public static Object[][] getLoginData3() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginData[] loginData = objectMapper.readValue(new File("resources/data.json"), LoginData[].class);
+        String username = loginData[2].getUsername();
+        String password = loginData[2].getPassword();
+        return new Object[][]{{username, password}};
+    }
+
+
+
+}
